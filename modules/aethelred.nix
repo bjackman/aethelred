1d10ba96872f3d00f7b54aa7b35b17acfe5f3c0c
Brendan Jackman <jackmanb@google.com>
2026-02-02 16:47:16 +0000

    Enable cdc_acm module

--- modules/aethelred.nix
+++ modules/aethelred.nix
@@ -12,4 +12,15 @@
     ./aethelred-hardware-configuration.nix
   ];
   networking.hostName = "aethelred";
+
+  # I have a PiKVM attached with Serial-over-USB enabled in kvmd as per
+  # https://docs.pikvm.org/usb_serial/.
+  # I have ensure the Pi isn't running Getty on that interface, which from its
+  # perspective will be called /dev/ttyGS0 (GS = "gadget serial").
+  # This is assuming the Aethelred kernel has CONFIG_USB_ACM.
+  # Should then be able to read this console output from the PiKVM with just cat
+  # /dev/ttyGS0.
+  boot.initrd.kernelModules = [ "cdc_acm" ];
+  # The below causes it to crash if uncommented.
+  # boot.kernelParams = [ "console=ttyACM0,115200" ];
 }
